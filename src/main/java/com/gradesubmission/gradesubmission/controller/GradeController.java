b0e9963eabc98163cdd4a9c104ce9f5469967d32
candegersen <candeger.sen@hotmail.com>
2024-10-18 21:24:07 +0300

    added service

--- src/main/java/com/gradesubmission/gradesubmission/controller/GradeController.java
+++ src/main/java/com/gradesubmission/gradesubmission/controller/GradeController.java
@@ -1,4 +1,4 @@
-package com.gradesubmission.gradesubmission;
+package com.gradesubmission.gradesubmission.controller;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.gradesubmission.gradesubmission.Constants;
+import com.gradesubmission.gradesubmission.Grade;
 
 @Controller
 public class GradeController {
